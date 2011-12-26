77f39e7c47afe843c7ac4dfc8d5a13da64104c02
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2011-12-26 13:48:54 +0300

    Implemented iterators methods for Base class: #each_fetch and #each_{aspect}

--- lib/rubyoverflow/base.rb
+++ lib/rubyoverflow/base.rb
@@ -5,25 +5,53 @@ module Rubyoverflow
     end
 
     def fetch(params = {})
-      ids = params.delete(:id) if params[:id]
-      ids = ids.join(';') if ids and ids.kind_of? Array
-      hash,url = @client.request "#{@path}#{"/#{ids}" if ids}", params
-      Hashie::Mash.new hash
+			get_response params
     end
+
+		def each_fetch(params = {}, &block)
+			get_each_response(params, &block)
+		end
 
     def method_missing(name, *args, &block)
-      params = args.first
-      ids = params.delete(:id) if params[:id]
-      ids = ids.join(';') if ids and ids.kind_of? Array
-      hash,url = @client.request "#{@path}#{"/#{ids}" if ids}/#{name}", params
-      Hashie::Mash.new hash
-    end
+			params = args.first
+			each_prefix = 'each_'
+			if name.to_s.start_with? each_prefix
+				aspect = name[each_prefix.length..-1]
+				get_each_response params, aspect, &block
+			else
+				get_response params, name
+			end
+		end
 
     protected
+
+		def get_response in_params, aspect = nil
+			params = in_params.clone # in order not to remove :id from original input parameter
+			ids = params.delete(:id) if params[:id]
+			ids = ids.join(';') if ids and ids.kind_of? Array
+			request_path = "#{@path}#{"/#{ids}" if ids}"
+			request_path << "/#{aspect}" if aspect
+			hash, url = @client.request request_path, params
+			Hashie::Mash.new hash
+		end
+
+		def get_each_response params, aspect = nil, &block
+			return enum_for(:each_fetch, params) unless block_given?
+
+			first_page_res = get_response(params, aspect)
+			yield first_page_res
+
+			total, first_page, pagesize = first_page_res.total, first_page_res.page, first_page_res.pagesize
+			return if pagesize == 0
+			last_page = (total / pagesize) + (total % pagesize != 0 ? 1 : 0)
+
+			(first_page+1..last_page).each { |p| yield get_response(params.merge(:page => p), aspect) }
+
+			self
+		end
 
     def set_path(pa)
       @path = pa
     end
-
   end
 end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -19,5 +19,79 @@ describe Users do
   it "retrieves multiple users by an array of ids" do
     result = @client.users.fetch(:id => [53587,22656])
     result.total.should == 2
-  end
+	end
+
+	context 'when request several pages' do
+		# methods are named according to Enumerable#each_* naming convention
+		describe '#each_fetch' do
+			it 'yields block for each page' do
+				paqes = []
+				@client.users.each_fetch(:id => [53587, 22656, 23354], :pagesize => 2) { |page_result|
+					paqes << page_result
+				}
+
+				paqes.should have(2).items
+
+				paqes[0].page.should == 1
+				paqes[1].page.should == 2
+			end
+
+			it 'should start iterating from :page parameter (when specified)' do
+				pages = []
+				@client.users.each_fetch(:id => [53587, 22656, 23354], :pagesize => 2, :page => 2) { |page_result|
+					pages << page_result
+				}
+
+				pages.should have(1).item
+
+				pages[0].page.should == 2
+			end
+
+			it 'should ignore requests with :pagesize = 0' do
+				expect { @client.users.each_fetch(:pagesize => 0) {} }.should_not raise_error
+			end
+
+			it 'should return self if block given' do
+				@client.users.each_fetch(:id => 53587) {}.should equal @client.users
+			end
+
+			it 'should return Enumerator if no block given' do
+				enum = @client.users.each_fetch(:id => 53587)
+
+				enum.should be_an_instance_of Enumerator
+
+				pages = []
+				enum.each { |page| pages << page }
+
+				pages.should have(1).page
+				pages[0].should have(1).users
+				pages[0].users[0].user_id.should == 53587
+			end
+		end
+
+		describe '#each_{method_missing}' do
+			def fake_page total, page = 1, pagesize = 30
+				{'total' => total, 'page' => page, 'pagesize' => pagesize }
+			end
+
+			let(:users_client) do
+				client = mock("Rubyoverflow::Client")
+				client.should_receive(:request).with('users/53587/questions', {}).and_return(fake_page(54))
+				client.should_receive(:request).with('users/53587/questions', {:page => 2}).and_return(fake_page(54, 2))
+				client
+
+				Rubyoverflow::Users.new(client)
+			end
+
+			it 'should iterate for each page' do
+				paqes = []
+				users_client.each_questions(:id => [53587]) {|page| paqes << page}
+
+				paqes.should have(2).items
+
+				paqes[0].page.should == 1
+				paqes[1].page.should == 2
+			end
+		end
+	end
 end
